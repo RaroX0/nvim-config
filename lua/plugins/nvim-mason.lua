8a2032b173fe229d7cf66795763da672f0816fdc
ray <66714046+RaroX0@users.noreply.github.com>
2026-01-12 08:56:54 +0530

    Update nvim-mason.lua

--- lua/plugins/nvim-mason.lua
+++ lua/plugins/nvim-mason.lua
@@ -11,7 +11,7 @@ return
 		config=function()
 			require("mason-lspconfig").setup({
 				ensure_installed={
-					"lua_ls", "rust_analyzer"
+					"lua_ls", "rust_analyzer", "clangd"
 				}
 			})
 		end
